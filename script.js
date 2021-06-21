32975387bbaacc1c8af8be49228069fae31f0e8e
SL <samanthl795@gmail.com>
2021-06-21 16:05:09 -0700

    Prompt user for the character types for the password

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Array of lower case letters
+var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// Array of upper case letters
+var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+// Array of numeric characters
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Special characters allowed for passwords from https://owasp.org/www-community/password-special-characters
+var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
 // Generate the password based on the criteria the user has selected
 function generatePassword() {
   // Get the number of characters that are desired for the password
@@ -9,6 +18,21 @@ function generatePassword() {
   // Make sure the number of characters is not null, NaN, less than 8, or greater than 128
   if (!numCharacters || (numCharacters < 8) || (numCharacters > 128)) {
     alert("Number of characters must be at least 8 characters and no more than 128 characters.");
+    return "";
+  }
+
+  // Ask the user if they want lowercase characters
+  var includeLowerCase = confirm("Press OK to include lower case characters in the password and CANCEL to not include them.");
+  // Ask the user if they want uppercase characters
+  var includeUpperCase = confirm("Press OK to include upper case characters in the password and CANCEL to not include them.");
+  // Ask the user if they want numeric characters
+  var includeNumeric = confirm("Press OK to include numeric characters in the password and CANCEL to not include them.");
+  // Ask the user if they want special characters
+  var includeSpecial = confirm("Press OK to include special characters in the password and CANCEL to not include them.");
+
+  // Make sure 1 character type is included
+  if (!includeLowerCase && !includeUpperCase && !includeNumeric && !includeSpecial) {
+    alert("You must select at least 1 character type");
     return "";
   }
 }
