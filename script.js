c209889dad3cc4a9bd4a11de7b6bdf93e522e214
SL <samanthl795@gmail.com>
2021-06-21 15:37:06 -0700

    Prompt users for the number of characters for the password

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Generate the password based on the criteria the user has selected
+function generatePassword() {
+  // Get the number of characters that are desired for the password
+  var numCharacters = prompt("Please enter the length of the password. It must be at least 8 characters and no more than 128 characters.");
+  numCharacters = parseInt(numCharacters);
+  // Make sure the number of characters is not null, NaN, less than 8, or greater than 128
+  if (!numCharacters || (numCharacters < 8) || (numCharacters > 128)) {
+    alert("Number of characters must be at least 8 characters and no more than 128 characters.");
+    return "";
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
