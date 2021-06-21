2e5844f79cdef98ed67fdc25f39c64bb9080db47
SL <samanthl795@gmail.com>
2021-06-21 16:54:46 -0700

    Add password generation

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // Special characters allowed for passwords from https://owasp.org/www-community/password-special-characters
 var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
+// Random integer function from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 // Generate the password based on the criteria the user has selected
 function generatePassword() {
   // Get the number of characters that are desired for the password
@@ -35,6 +40,51 @@ function generatePassword() {
     alert("You must select at least 1 character type");
     return "";
   }
+
+  // Characters to choose from for the password
+  var charactersToChooseFrom = [];
+  // Characters chosen for the password
+  var charactersChosenForPassword = [];
+
+  if (includeLowerCase) {
+    // Add the lower case characters to the characters to choose from
+    charactersToChooseFrom = charactersToChooseFrom.concat(lowerCaseLetters);
+    // Choose a lower case character so there is at least one in the password
+    charactersChosenForPassword.push(lowerCaseLetters[getRandomInt(lowerCaseLetters.length)]);
+    numCharacters--;
+  }
+
+  if (includeUpperCase) {
+    // Add the upper case characters to the characters to choose from
+    charactersToChooseFrom = charactersToChooseFrom.concat(upperCaseLetters);
+    // Choose a upper case character so there is at least one in the password
+    charactersChosenForPassword.push(upperCaseLetters[getRandomInt(upperCaseLetters.length)]);
+    numCharacters--;
+  }
+
+  if (includeNumeric) {
+    // Add the numeric characters to the characters to choose from
+    charactersToChooseFrom = charactersToChooseFrom.concat(numericCharacters);
+    // Choose a numeric character so there is at least one in the password
+    charactersChosenForPassword.push(numericCharacters[getRandomInt(numericCharacters.length)]);
+    numCharacters--;
+  }
+
+  if (includeSpecial) {
+    // Add the special characters to the characters to choose from
+    charactersToChooseFrom = charactersToChooseFrom.concat(specialCharacters);
+    // Choose a special character so there is at least one in the password
+    charactersChosenForPassword.push(specialCharacters[getRandomInt(specialCharacters.length)]);
+    numCharacters--;
+  }
+
+  // Choose the remaining characters
+  for (var i = 0; i < numCharacters; i++) {
+    charactersChosenForPassword.push(charactersToChooseFrom[getRandomInt(charactersToChooseFrom.length)]);
+  }
+
+  // Return the chosen characters as one string
+  return charactersChosenForPassword.join('');
 }
 
 // Write password to the #password input
